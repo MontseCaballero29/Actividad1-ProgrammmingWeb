526ca5378184a5b069deb77d339888a70516b2ca
Dalia Montserrat Caballero Silva <151937757+MontseCaballero29@users.noreply.github.com>
2026-07-03 10:39:30 -0600

    error

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,12 +1,14 @@
 function convertir() {
-    let celsius = document.getElementById("celsius").value;
+    let celsius = document.getElementById("numeroConvertir").value;
 
     if (celsius === "" || isNaN(celsius)) {
         alert("Ingresa un valor numérico válido");
         return;
     }
 
+    celsius = parseFloat(celsius);
+
     let fahrenheit = (celsius * 9 / 5) + 32;
 
-    document.getElementById("fahrenheit").value = fahrenheit + "°F";
+    document.getElementById("numeroConvertido").value = fahrenheit + "°F";
 }
